733541c31537da9aaca0922296f2a73fde77bdbf
jordan <email@mail.com>
2022-03-15 09:01:21 -0300

    Se añade ejemplo de Fracción.

---
+++ Ejemplo_TAD_Fraccion/fraccion.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 07:59:15 2022
+
+@author: jorda
+"""
+
+def mcd(a, b):
+    '''
+    Obtiene el máximo común divisor (MCD) entre dos números aplicando el algoritmo de Euclides.
+    Parameters
+    ----------
+    a : Entero positivo.
+    b : Entero positivo.
+    Returns
+    -------
+    a : Entero positivo.
+        Es el máximo común divisor calculado.
+    '''
+    while b:
+        a, b = b, a % b
+    return a
+
+
+class Fraccion:
+    def __init__(self,arriba,abajo):
+        self.num = arriba
+        self.den = abajo
+        
+    def __str__(self):
+        cad = str(self.num)
+        if self.den != 1:
+            cad += "/"+str(self.den)
+        return cad
+        
+    def __add__(self,otraFraccion):
+        nuevoNum = self.num*otraFraccion.den + self.den*otraFraccion.num
+        nuevoDen = self.den * otraFraccion.den
+        comun = mcd(nuevoNum,nuevoDen) #algoritmo de Euclides
+        return Fraccion(nuevoNum//comun,nuevoDen//comun)
---
+++ Ejemplo_TAD_Fraccion/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 08:04:43 2022
+
+@author: jorda
+"""
+
+from fraccion import Fraccion
+
+miFraccion = Fraccion(3,5)
+
+f1 = Fraccion(1,4)
+f2 = Fraccion(1,2)
+
+f3 = f1 + f2
+
+f1 = Fraccion(10,4)
+f2 = Fraccion(1,2)
+
+f3 = f1 + f2
+
+print(f3)
